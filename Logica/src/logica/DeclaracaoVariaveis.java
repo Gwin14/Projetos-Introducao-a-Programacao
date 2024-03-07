d7e8a8678cd25854189a4613db5da608dbb73c1e
Fábio Santos <fabiospinheira@gmail.com>
2024-03-06 21:50:41 -0300

    DeclaracaoVariaveis

---
+++ Logica/src/logica/DeclaracaoVariaveis.java
@@ -0,0 +1,19 @@
+package logica;
+
+public class DeclaracaoVariaveis {
+
+    public static void main(String[] args) {
+
+        String nome = "José";
+        int idade = 40;
+        String rg = "4324099";
+        char sexo = 'M';
+        double salario = 2100.30;
+
+        System.out.println("Senhor(a) " + nome + ", de " + idade
+                + " anos, portador do RG de número " + rg + ", do sexo " + sexo
+                + ", está registrado com salário de " + salario + ".");
+
+    }
+
+}
