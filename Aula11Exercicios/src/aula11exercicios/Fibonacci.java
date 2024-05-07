fe086cf1b00a68fb91bacd11aab595fbbe913611
Fábio Santos <fabiospinheira@gmail.com>
2024-05-07 20:55:22 -0300

    Create Fibonacci.java

---
+++ Aula11Exercicios/src/aula11exercicios/Fibonacci.java
@@ -0,0 +1,24 @@
+package aula11exercicios;
+
+public class Fibonacci {
+
+    public static void main(String[] args) {
+
+        int num1 = 0;
+        int num2 = 1;
+        int num3;
+
+        System.out.println(num1);
+        System.out.println(num2);
+
+        for (int i = 0; i < 20; i++) {
+            num3 = num1 + num2;
+
+            System.out.println(num3);
+
+            num1 = num2;
+            num2 = num3;
+        }
+    }
+
+}
